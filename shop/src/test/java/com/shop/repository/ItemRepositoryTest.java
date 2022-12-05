0d99169e48bffe70ba10c3af398a2447f6921609
eunbeann <ds5bean23@gmail.com>
2022-12-05 16:47:21 +0900

    p.104 QItem error fix

--- shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -125,13 +126,22 @@ class ItemRepositoryTest {
             System.out.println(item.toString());
         }
     }
-
     @Test
     @DisplayName("Querydsl 조회 테스트1")
     public void queryDslTest() {
         this.createItemList();
         JPAQueryFactory queryFactory = new JPAQueryFactory(em);
         QItem qItem = QItem.item;
-        JPAQuery<Item> query = queryFactory.selectFrom(qItem).where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+        List<Item> itemList = query.fetch();
+
+        for(Item item : itemList) {
+            System.out.println(item.toString());
+        }
+
     }
+
 }
